e0f3a1ecc20e1ff5ca893a9bcebc01a2b3f103ee
Leo jPod <leo.jpod@lilo.org>
2020-10-28 13:38:36 +0100

    add some docs

--- src/Shortcut.elm
+++ src/Shortcut.elm
@@ -1,10 +1,58 @@
 module Shortcut exposing
     ( Key(..)
-    , esc
-    , shiftShortcut
-    , shortcutElement
+    , Shortcut
     , simpleShortcut
+    , esc
+    , altShortcut
+    , shiftShortcut
+    , ctrlShortcut
+    , metaShortcut
+    , shortcutElement
     )
+
+{-| This module provide a quick and simple (enough) way of listening for keyboard event by describing which shortcut you are interested in.
+
+
+# Shortcut description
+
+The first important part of using this package is describing which shortcut should trigger which message.
+This can be done by instantiating the `Shortcut` record directly or via one of the utility function
+
+
+## Keys
+
+@docs Key
+
+
+## Shortcut record
+
+In essence, a shortcut is just an indication of which key(s) we are interested in and a message that should be sent when a keyboard event matches out.
+
+@docs Shortcut
+
+
+## Utility functions
+
+@docs simpleShortcut
+
+@docs esc
+
+@docs altShortcut
+
+@docs shiftShortcut
+
+@docs ctrlShortcut
+
+@docs metaShortcut
+
+
+# Shortcut capture
+
+The capture part is taken care of by the custom-element that comes with this package. For a reminder on how to install it, please have a look at the [README](./).
+
+@docs shortcutElement
+
+-}
 
 import Html exposing (Html, node)
 import Html.Attributes
@@ -16,6 +64,9 @@ import List.Extra
 import Maybe.Extra
 
 
+{-| The `Key` is merely an easy way to define which key you are interested in.
+It has an entry for all the special keys and a constructor (`Regular`) for all the rest.
+-}
 type Key
     = Escape
     | BackSpace
@@ -37,6 +88,21 @@ type Key
     | Regular String
 
 
+{-| Shortcut is simply a record of a message and a keyCombination that describe the keyboard shortcut you're interested in.
+
+Please note that the `Maybe Bool` for the alt, shift, ctrl and meta modifirs allows you to choose if you would like to ignore a modifier (`Nothing`), force it to be present (`True`) or force it to be abscent (`False`)
+
+For instance the following key combination will match both _Shift_ _O_ and _Ctrl_ _Shift_ _O_ but not _Shift_ _Meta_ _O_
+
+    myKeyCombination =
+        { baseKey = Regular "O"
+        , alt = Nothing
+        , shift = Just True
+        , ctrl = Nothing
+        , meta = Just False
+        }
+
+-}
 type alias Shortcut msg =
     { msg : msg
     , keyCombination :
@@ -49,6 +115,24 @@ type alias Shortcut msg =
     }
 
 
+{-| if you're only interested in one key not in any of the modifiers then `simpleShortcut` is made for you.
+Both of these are equivalent:
+
+    myShortcut =
+        { msg = SimpleMessage
+        , keyCombination =
+            { baseKey = Regular "O"
+            , alt = Nothing
+            , shift = Nothing
+            , ctrl = Nothing
+            , meta = Nothing
+            }
+        }
+
+    mySimpleShortcut =
+        simpleShortcut <| Regular "O"
+
+-}
 simpleShortcut : Key -> msg -> Shortcut msg
 simpleShortcut key msg =
     { msg = msg
@@ -62,11 +146,30 @@ simpleShortcut key msg =
     }
 
 
+{-| alias of `simpleShortcut Escape`
+-}
 esc : msg -> Shortcut msg
 esc =
     simpleShortcut Escape
 
 
+{-| equivalent of setting all the modifiers to `Nothing` and the **alt** modifier to `Just True`
+-}
+altShortcut : Key -> msg -> Shortcut msg
+altShortcut key msg =
+    { msg = msg
+    , keyCombination =
+        { baseKey = key
+        , alt = Just True
+        , shift = Nothing
+        , ctrl = Nothing
+        , meta = Nothing
+        }
+    }
+
+
+{-| equivalent of setting all the modifiers to `Nothing` and the **shift** modifier to `Just True`
+-}
 shiftShortcut : Key -> msg -> Shortcut msg
 shiftShortcut key msg =
     { msg = msg
@@ -80,6 +183,38 @@ shiftShortcut key msg =
     }
 
 
+{-| equivalent of setting all the modifiers to `Nothing` and the **ctrl** modifier to `Just True`
+-}
+ctrlShortcut : Key -> msg -> Shortcut msg
+ctrlShortcut key msg =
+    { msg = msg
+    , keyCombination =
+        { baseKey = key
+        , alt = Nothing
+        , shift = Nothing
+        , ctrl = Just True
+        , meta = Nothing
+        }
+    }
+
+
+{-| equivalent of setting all the modifiers to `Nothing` and the **meta** modifier to `Just True`
+-}
+metaShortcut : Key -> msg -> Shortcut msg
+metaShortcut key msg =
+    { msg = msg
+    , keyCombination =
+        { baseKey = key
+        , alt = Nothing
+        , shift = Nothing
+        , ctrl = Nothing
+        , meta = Just True
+        }
+    }
+
+
+{-| utility function to prepare encoding of the keys
+-}
 keyToString : Key -> String
 keyToString key =
     case key of
@@ -138,6 +273,8 @@ keyToString key =
             keyName
 
 
+{-| utility function to create an identity to each of the shortcut so we can find out which of them was triggered when we hear back from the custom element
+-}
 hashShortcut : Shortcut msg -> String
 hashShortcut { keyCombination } =
     keyToString keyCombination.baseKey
@@ -179,6 +316,8 @@ hashShortcut { keyCombination } =
             keyCombination.shift
 
 
+{-| the classical JSON encoder for a given shortcut
+-}
 encodeShortcut : Shortcut msg -> Json.Encode.Value
 encodeShortcut ({ keyCombination } as shortcut) =
     Json.Encode.object
@@ -191,6 +330,8 @@ encodeShortcut ({ keyCombination } as shortcut) =
         ]
 
 
+{-| our custom event listener that will figure out which of the given shortcut was called
+-}
 onShortcut : List (Shortcut msg) -> Html.Attribute msg
 onShortcut shortcuts =
     Html.Events.on "shortcut"
@@ -203,6 +344,8 @@ onShortcut shortcuts =
         )
 
 
+{-| This is the elm wrapper for the custom-element provided by the NPM companion package
+-}
 shortcutElement : List (Shortcut msg) -> List (Html.Attribute msg) -> List (Html msg) -> Html msg
 shortcutElement shortcuts attrs =
     node "shortcut-element"
